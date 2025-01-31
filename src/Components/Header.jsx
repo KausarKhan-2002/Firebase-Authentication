2335c304fa986f46b27a71221b91bfe959af86db
Kausar Khan <kausarrkhan83@gmail.com>
2025-01-31 19:40:41 +0530

    Remove username and profile during signin or signup activities using useLocation hook

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
   const user = useSelector((store) => store.user);
+  const location = useLocation();
 
   return (
     <div className="shadow-md py-5 font-bold text-xl px-10 flex items-center justify-between">
       <div id="logo">
         My<span className="text-red-700">Logo</span>
       </div>
-      {user && (
+      {user && location.pathname === "/browse" && (
         <div className="flex items-center gap-2">
           <h3>{user.displayName}</h3>
           <img
